cd25baaf18c08065797941d2b1591adf9b652e5d
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2021-01-01 22:36:51 +0530

    yeah
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

---
+++ something.rb
@@ -0,0 +1,17 @@
+class Something < Formula
+  desc "something to test homebrew"
+  homepage "harikrishnanbalagopal.github.io/move2kube"
+  url "https://github.com/HarikrishnanBalagopal/something/archive/v0.1.0.tar.gz"
+  sha256 "01401d7f7dd38d85b6d29c9a4420cb4993deedb3b57ab1d51d32c78666fb2cc9"
+  license "Apache-2.0"
+
+  depends_on "go@1.15" => :build
+
+  def install
+    system "make", "build"
+  end
+
+  test do
+    system "#{bin}/dist/something"
+  end
+end
