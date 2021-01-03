ace952a2bc8fb7e4964545ff5b142c2bb96e625b
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2021-01-03 21:17:57 +0530

    ssss: sssss
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

--- something.rb
+++ something.rb
@@ -1,9 +1,15 @@
 class Something < Formula
   desc "To test homebrew"
   homepage "https://harikrishnanbalagopal.github.io/move2kube"
-  url "https://github.com/HarikrishnanBalagopal/something/archive/v0.1.0.tar.gz"
-  sha256 "01401d7f7dd38d85b6d29c9a4420cb4993deedb3b57ab1d51d32c78666fb2cc9"
   license "Apache-2.0"
+  head "https://github.com/HarikrishnanBalagopal/something.git"
+
+  stable do
+  url "https://github.com/HarikrishnanBalagopal/something.git",
+    using: :git,
+    tag: "v0.1.1"
+    revision: "2a4a7c6f2428bdf33ad641b7d3ad46c8e63ea7a8"
+  end
 
   bottle do
     root_url "https://github.com/HarikrishnanBalagopal/homebrew-something/releases/download/something-0.1.0"
